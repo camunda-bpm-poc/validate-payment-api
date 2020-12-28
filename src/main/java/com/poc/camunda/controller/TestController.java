e65bf8f18cc261e813a197b942c315b0a27db936
Mohammed Sohail <mohammed.sohail@cgius.com>
2020-12-28 12:14:26 -0500

    Initial Commit - 12/28

---
+++ src/main/java/com/poc/camunda/controller/TestController.java
@@ -0,0 +1,57 @@
+package com.poc.camunda.controller;
+
+import com.sun.corba.se.impl.protocol.giopmsgheaders.RequestMessage;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.poc.camunda.controller.CamundaStartService;
+import org.json.*;
+@RestController
+
+@RequestMapping("/poc")
+public class TestController {
+    @Autowired
+    CamundaStartService camundaStartService;
+    @GetMapping("/test")
+    public String testMethod(){
+        RuntimeService runtimeService = null;
+        //ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("my-project-process");
+        return "test";
+    }
+
+    @RequestMapping(value = "/startProcess", method = RequestMethod.POST)
+    public void startProcess(@RequestBody String paymentInfo){
+        String jsonStr = paymentInfo;
+        JSONObject obj = new JSONObject(jsonStr);
+        String ssn = "";
+        String accountNumber = "";
+        String paymentAmount = "";
+        String paymentDate = "";
+        System.out.println("paymentInfo:"+paymentInfo);
+        ssn = obj.getJSONObject("paymentInfo").getString("ssn");
+        accountNumber = obj.getJSONObject("paymentInfo").getString("accountNumber");
+        paymentAmount = obj.getJSONObject("paymentInfo").getString("paymentAmount");
+        paymentDate = obj.getJSONObject("paymentInfo").getString("paymentDate");
+        System.out.println(ssn+" "+accountNumber+" "+paymentAmount+" "+paymentDate);
+        camundaStartService.startProcessByMessage(ssn,accountNumber, paymentAmount, paymentDate);
+    }
+
+    @RequestMapping(value = "/isPaymentValid/{paymentAmount}", method = RequestMethod.GET)
+    public String isPaymentValid(@PathVariable String paymentAmount){
+        Integer paymentAmt = Integer.parseInt(paymentAmount);
+        if(paymentAmt == 100)
+                return "true";
+        else
+                return "false";
+    }
+    @RequestMapping(value = "/sentPaymentValidStatus", method = RequestMethod.POST)
+    public void resumeProcess(@RequestBody String paymentStatus){
+        String jsonStr = paymentStatus;
+        JSONObject obj = new JSONObject(jsonStr);
+        String correlationID = obj.getJSONObject("paymentStatus").getString("correlationID");
+        String isValid = obj.getJSONObject("paymentStatus").getString("isValid");
+        camundaStartService.resumeProcess(correlationID,isValid);
+    }
+
+}
